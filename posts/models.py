be15d4037803ee914147dd9c8a037b21ef6d1cd4
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-04-26 18:06:11 -0300

    templatetag for markdown, hope it works, and basic template for html

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=123)
+
+    def __str__(self):
+        return f"{self.id}, {self.name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.OneToOneField(User, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.tags}, {self.publish_date}"
