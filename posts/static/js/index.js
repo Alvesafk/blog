73838692b8071e8f965895d7d11c3c86494c890f
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-04-28 19:54:18 -0300

    js event listener to take you the most recent post

--- posts/static/js/index.js
+++ posts/static/js/index.js
@@ -2,10 +2,18 @@
 
 const aboutLink = document.getElementById("about-link");
 const postsLink = document.getElementById("posts-link");
+const post = document.getElementById("recent-post");
 
 function setupEventListeners() {
 	if(aboutLink) {
 		aboutLink.classList.add("is-disabled");
+	}
+
+	if(post) {
+		post.addEventListener('click', () => {
+			const postId = post.getAttribute('data-id');
+			window.location.href = `/posts/${postId}`;
+		});
 	}
 }
 
