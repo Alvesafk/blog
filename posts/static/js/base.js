bd20652ed521c8229ae8bdb0288a493b8add3d06
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-02 22:22:01 -0300

    beep sound added

--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -9,7 +9,11 @@ const observer = new PerformanceObserver((list) => {
 	const totalTime = (lastEntry.loadEventEnd - lastEntry.startTime) / 1000;
 	const formattedTime = totalTime.toFixed(2);
 
-	timeStat.textContent = `The page was loaded in ${formattedTime} seconds!`;
+	if(formattedTime > 1) {
+		timeStat.textContent = `The page as loaded in ${formattedTime}... so slow! -_-`
+	} else if(formattedTime < 1) {
+		timeStat.textContent = `The page was loaded in ${formattedTime} seconds, so fast!`;
+	}
 });
 
 observer.observe({ type: 'navigation', buffered: true });
