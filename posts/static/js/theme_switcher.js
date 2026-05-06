3659951e943b75b72121e7f236b2d45b2f12455b
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-05 22:08:52 -0300

    new theme switcher, now gonna add the other themes

--- posts/static/js/theme_switcher.js
+++ posts/static/js/theme_switcher.js
@@ -1,30 +1,11 @@
 'use strict';
 
-const btn = document.getElementById('theme-icon-div');
-let currentTheme = document.documentElement.getAttribute("data-theme");
+const allBtns = document.querySelectorAll('.theme-button');
 
-function changeThemeIcon() {
-	currentTheme = document.documentElement.getAttribute("data-theme");
-
-	const isDark = currentTheme === "dark";
-	const iconAdd = isDark ? "fa-moon" : "fa-sun";
-	const iconRemove = !isDark ? "fa-moon" : "fa-sun";
-
-	const themeIcon = document.getElementById("theme-icon");
-
-	themeIcon.classList.add(iconAdd);
-	themeIcon.classList.remove(iconRemove);
-}
-
-btn.addEventListener("click", function() {
-    let theme = document.documentElement.getAttribute("data-theme");
-    let targetTheme = (theme === "dark") ? "light" : "dark";
-
-    document.documentElement.setAttribute("data-theme", targetTheme);
-    
-    document.cookie = `theme=${targetTheme}; path=/; max-age=${60*60*24*365}`;
-
-	changeThemeIcon();
+allBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		const theme = btn.getAttribute('data-theme-name');
+		document.documentElement.setAttribute('data-theme', theme);
+		document.cookie = `theme=${theme}; path=/; max-age=${60*60*24*365}`;
+	});
 });
-
-changeThemeIcon();
