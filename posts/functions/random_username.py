04057e7610749f9dc627acfbf9d1bbe17a430b1d
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-07 22:00:43 -0300

    more names for the random username array, now 27000 different names can be generated

--- posts/functions/random_username.py
+++ posts/functions/random_username.py
@@ -2,41 +2,18 @@ import random
 
 def get_random_username():
     prefix = [
-        "Dark",
-        "Silent",
-        "Toxic",
-        "Hyper",
-        "Shadow",
-        "Frost",
-        "Iron",
-        "Blazing",
-        "Savage",
-        "Neon"
-    ] 
-
-    middle = [
-        "Wolf",
-        "Blade",
-        "Skull",
-        "Viper",
-        "Storm",
-        "Ghost",
-        "Reaper",
-        "Hawk",
-        "Phantom",
-        "Demon",
+        "Dark", "Silent", "Toxic", "Hyper", "Shadow", "Frost", "Iron", "Blazing", "Savage", "Neon",
+        "Crimson", "Venom", "Cyber", "Omega", "Void", "Steel", "Chaos", "Blood", "Grim", "Turbo",
+        "Death", "Night", "Inferno", "Rogue", "Storm", "Ultra", "Wicked", "Zero", "Blaze", "Ghost"
     ]
-
+    middle = [
+        "Wolf", "Blade", "Skull", "Viper", "Storm", "Ghost", "Reaper", "Hawk", "Phantom", "Demon",
+        "Knight", "Raven", "Dragon", "Sniper", "Titan", "Cobra", "Specter", "Wraith", "Striker", "Hunter",
+        "Banshee", "Predator", "Ronin", "Jackal", "Phoenix", "Slayer", "Golem", "Lynx", "Tempest", "Shade"
+    ]
     sufix = [
-        "360",
-        "X",
-        "Pro",
-        "HD",
-        "99",
-        "Xx",
-        "II",
-        "Zz",
-        "666",
-        "Elite",
+        "360", "X", "Pro", "HD", "99", "Xx", "II", "Zz", "666", "Elite",
+        "V2", "Max", "Ultra", "OG", "420", "Zero", "Alpha", "Omega", "4K", "EXE",
+        "OP", "XD", "777", "God", "Senpai", "NoScope", "1337", "AFK", "GG", "Boss"
     ]
     return f"{prefix[random.randrange(0, len(prefix) - 1)]}{middle[random.randrange(0, len(middle) - 1)]}{sufix[random.randrange(0, len(sufix) - 1)]}"
